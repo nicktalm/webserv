f1f8ac00b321334bb5c6f74353e385d467866a0b
Your Name <you@example.com>
2025-03-14 14:40:44 +0100

    basic main structure finished / check_config needs to be finished

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -7,3 +7,17 @@
 #define BLUE    "\033[34m"
 #define YELLOW  "\033[33m"
 #define RESET   "\033[0m"
+
+
+class Server{
+	private:
+		int socketfd;
+		int port;
+
+	public:
+		int get_socketfd(){return socketfd;};
+		int get_port(){return port;};
+		void initServer();
+};
+
+//gerne anpassen, ich glaube wir brauchen die values aber nicht sicher
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,14 +1,35 @@
 #include "../include/webserv.hpp"
 
 
-
+bool check_config(std::string config_path){
+	(void)config_path;
+	return true;
+}
 
 int main(int argc, char **argv){
-	(void)argv;
-	if (argc != 2){
+	if (argc > 2){
 		std::cout << RED << "Wrong number of arguments, try [./webserv configuration_file]" << RESET << std::endl;
 		return EXIT_FAILURE;
 	}
-	std::cout << GREEN << "Starting server..." << RESET << std::endl;
+	else{
+		std::string config_file;
+		if (argc == 2)
+			config_file = argv[1];
+		else
+			config_file = "/default/default.conf";
+		if (!check_config(config_file)){
+			std::cout << RED << "Error in config file" << RESET << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	try{
+		Server server;
+		std::cout << GREEN << "Starting server..." << RESET << std::endl;
+		server.initServer();//Startpunkt fuer alles weitere
+	}
+	catch (std::exception &e){
+		std::cout << RED << e.what() << RESET << std::endl;
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
